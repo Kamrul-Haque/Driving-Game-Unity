87b66ac1401a1c2da3b33977d7828e2e0b9f49ad
Kamrul-Haque <kamrul35-255@diu.edu.bd>
2021-11-13 21:43:53 +0600

    Backing Up

---
+++ Assets/Scripts/driving.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driving : MonoBehaviour
+{
+    const float MoveUnitPerSecond = 3f;
+
+    float colliderHalfWidth;
+    float colliderHalfHeght;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x / 2;
+        colliderHalfHeght = collider.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (horizontalInput != 0)
+        {
+            position.x += horizontalInput * MoveUnitPerSecond * Time.deltaTime;
+        }
+        if (verticalInput != 0)
+        {
+            position.y += verticalInput * MoveUnitPerSecond * Time.deltaTime;
+        }
+        transform.position = position;
+    }
+}
